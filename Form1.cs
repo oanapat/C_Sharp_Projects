8a487749793210116c87890bce37a20283309e34
Oana Patrascu <oanapat@users.noreply.github.com>
2016-06-22 13:22:20 -0700

    C#_Project2
    Store employee information in three objects and display data.

--- Form1.cs
+++ Form1.cs
@@ -8,61 +8,77 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-//Oana Patrascu Stadium Exercise
 
-//There are three seating categories at an athletic stadium. For a baseball game, Class
-//A seats cost $15 each, Class B seats cost $12 each, and Class C seats cost $9 each.
-//This program allows the user to enter the number of tickets sold for
-//each class and display the amount of income generated from each class of ticket sales 
-//as well as the total revenue generated.
+//Oana Patrascu
+//Entering employee Information
 
-namespace op_assignment2
+namespace EmployeeInfo
 {
-    public partial class stadium : Form
+    public partial class Form1 : Form
     {
-        public stadium()
+        //List to hold the Employee Inputs
+        List<Employee> employeeList = new List<Employee>();
+
+
+        public Form1()
         {
             InitializeComponent();
         }
 
-        private void revenueCalc_Click(object sender, EventArgs e)
+        //The method assigns the data entered by the user to the object's properties.
+        private void GetEmployeeInfo(Employee employeeInfo)
         {
-            //This is where we declare our variables
+            //temporary variable to hold the ID
+            int employeeID;
 
-            int outputA;
-            int outputB;
-            int outputC;
-            int Total;
+            //Get the Employee Name
+            employeeInfo.Name = nameTextBox.Text;
 
-            //This is where we do our calculations
-            outputA = int.Parse(textBoxA.Text) * 15;
-            outputB = int.Parse(textBoxC.Text) * 12;
-            outputC = int.Parse(textBoxB.Text) * 9;
-            Total = outputA + outputB + outputC;
-            
-            //This is where we display our result
-            labelA.Text = outputA.ToString("c");
-            labelB.Text = outputB.ToString("c");
-            labelC.Text = outputC.ToString("c");
-            totalLabel.Text = Total.ToString("c");
-            
+            //get the Employee ID number
+
+            if (int.TryParse(idNumberTextBox.Text, out employeeID))
+            {
+                employeeInfo.IdNumber = employeeID;
+            }
+
+            else
+            {
+                MessageBox.Show("The employee ID you entered is invalid");
+            }
+
+            //get the Employee Department
+            employeeInfo.Department = departmentTextBox.Text;
+
+            //get the Employee Position
+            employeeInfo.Position = positionTextBox.Text;
+
         }
-
-        //This is where we clear our boxes and labels
-        private void clear_Click(object sender, EventArgs e)
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            textBoxA.Text = " ";
-            textBoxB.Text = " ";
-            textBoxC.Text = " ";
-
-            labelA.Text = " ";
-            labelB.Text = " ";
-            labelC.Text = " ";
-            totalLabel.Text = " ";
 
         }
 
-        //This is where we exit our program
+        private void informationButton_Click(object sender, EventArgs e)
+        {
+            //Create a Employee object
+            Employee infoEmployee = new Employee();
+
+            GetEmployeeInfo(infoEmployee);
+
+            //adding the object to the list
+            employeeList.Add(infoEmployee);
+
+            //adding an entry to the list box
+            employeelistBox.Items.Add(infoEmployee.Name + " " + infoEmployee.Department + " " +
+                infoEmployee.Position + " " + infoEmployee.IdNumber);
+
+            //Clear the Text boxes
+            nameTextBox.Clear();
+            idNumberTextBox.Clear();
+            departmentTextBox.Clear();
+            positionTextBox.Clear();
+        }
+
         private void exitButton_Click(object sender, EventArgs e)
         {
             this.Close();
